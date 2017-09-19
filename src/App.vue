2aae3af70ef244bc869480df80ab59a33ec5f71f
Salvador Rocha Robledo <srocha@crisa.com.mx>
2017-09-19 17:22:31 -0500

    Issue Aside

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@
     <main class="main">
       <app-content v-for="item in data.Contents" :key="item.idContent" :text="item.contentText" :title="item.title"></app-content>
     </main>
+
+    <!-- <app-menu></app-menu> -->
   </div>
 </template>
 
@@ -18,6 +20,7 @@ import appHeader from './components/app-header/appHeader.vue';
 import appSocial from './components/app-social/appSocial.vue';
 import appContent from './components/app-content/appContent.vue';
 import appAside from './components/app-aside/appAside.vue';
+// import appMenu from './components/app-menu/appMenu.vue';
 
 export default {
   name: 'app',
@@ -29,15 +32,15 @@ export default {
     }
   },
   mounted() {
-    console.warn('Created: ' + moment().format('HH:mm:ss'));
+    //console.warn('Created: ' + moment().format('HH:mm:ss'));
     axios.get('http://bossinovations.com.stem.arvixe.com/api/Usuario/2')
       .then((response) => {
-        console.log(response.data);
+        //console.log(response.data);
         this.data = response.data;
-        console.warn('data:' + moment().format('HH:mm:ss'));
+        //console.warn('data:' + moment().format('HH:mm:ss'));
       })
       .catch((response) => {
-        console.log(reponse);
+        //console.log(reponse);
       });
   }
 }
---
+++ src/components/app-menu/appMenu.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="menu">
+    <a href="#" class="menu__link icon-user"></a>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
